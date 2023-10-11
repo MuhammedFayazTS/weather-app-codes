72fdb42e40a3d60f66a3109acc96d55d487087d1
MUHAMMED FAYAZ T S <139971477+MuhammedFayazTS@users.noreply.github.com>
2023-10-11 11:17:14 +0530

    Update TopSection.jsx

--- src/Components/TopSection.jsx
+++ src/Components/TopSection.jsx
@@ -1,41 +1,42 @@
-import { Button, Grid } from '@mui/material'
-import React from 'react'
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import SearchIcon from '@mui/icons-material/Search';
-import { blue} from '@mui/material/colors';
+Not needed Because data cannot be passed from search bar to app.js
+// import { Button, Grid } from '@mui/material'
+// import React from 'react'
+// import Typography from '@mui/material/Typography';
+// import TextField from '@mui/material/TextField';
+// import SearchIcon from '@mui/icons-material/Search';
+// import { blue} from '@mui/material/colors';
 
 
 
-function TopSection() {
-  return (
-    <div>
-        <Grid container  spacing={2} pr={2}>
+// function TopSection() {
+//   return (
+//     <div>
+//         <Grid container  spacing={2} pr={2}>
 
-            <Grid item xs={12} md={5}>
-            <Typography textAlign={'center'} fontWeight={700} fontSize={28} letterSpacing={1} variant="h5" color={'#3E3E3E'}>
-        Weather App
-      </Typography>
-            </Grid>
-            <Grid item xs={12} md={7}>
-                {/* search bar */}
-            <Grid container columnGap={1}>
-                <Grid xs={10} ml={1}>
-                    <TextField  id="filled-basic" label="City Name" sx={{bgcolor:'#fff'}} color="primary"  size="small" fullWidth />
-                </Grid>
-                <Grid xs={1}>
-                <Button sx={{bgcolor:blue[500]}} variant="contained">
-                    <SearchIcon />
-                </Button>
-                </Grid>
-            </Grid>
-            {/* searchbar end */}
-            </Grid>
+//             <Grid item xs={12} md={5}>
+//             <Typography textAlign={'center'} fontWeight={700} fontSize={28} letterSpacing={1} variant="h5" color={'#3E3E3E'}>
+//         Weather App
+//       </Typography>
+//             </Grid>
+//             <Grid item xs={12} md={7}>
+//                 {/* search bar */}
+//             <Grid container columnGap={1}>
+//                 <Grid xs={10} ml={1}>
+//                     <TextField  id="filled-basic" label="City Name" sx={{bgcolor:'#fff'}} color="primary"  size="small" fullWidth />
+//                 </Grid>
+//                 <Grid xs={1}>
+//                 <Button sx={{bgcolor:blue[500]}} variant="contained">
+//                     <SearchIcon />
+//                 </Button>
+//                 </Grid>
+//             </Grid>
+//             {/* searchbar end */}
+//             </Grid>
 
-        </Grid>
+//         </Grid>
         
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default TopSection
+// export default TopSection
